22199fb587f38711f602ef0f15ed3beb06229d93
voron5096 <70198995+voron5096@users.noreply.github.com>
2023-04-15 20:51:55 +0300

    Update main.cpp

--- КВ_1/main.cpp
+++ КВ_1/main.cpp
@@ -1,3 +1,4 @@
+#include "cl_application.h"
 int main () {
     cl_application  ob_cl_application ( nullptr ); // создание корневого объекта
     ob_cl_application.build_tree_objects ( );       // конструирование системы, построение дерева объектов
