333e5b2aaf2664d734b2052a9855b07652d0fc3a
voron5096 <70198995+voron5096@users.noreply.github.com>
2023-04-15 20:51:37 +0300

    Create main.cpp

---
+++ КВ_1/main.cpp
@@ -0,0 +1,5 @@
+int main () {
+    cl_application  ob_cl_application ( nullptr ); // создание корневого объекта
+    ob_cl_application.build_tree_objects ( );       // конструирование системы, построение дерева объектов
+    return ob_cl_application.exec_app ( );         // запуск системы
+}
