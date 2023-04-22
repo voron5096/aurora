aea8f6ed05d6f62e47576926c9d1b17053bb999b
voron5096 <70198995+voron5096@users.noreply.github.com>
2023-04-22 15:28:44 +0000

    4.1.1

---
+++ 4.1.1/main.cpp
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+// Разработать производный класс используя класс cl_parent в качестве родительского
+
+int main() {
+  int iPublic, iPrivat;
+  cin >> iPublic >> iPrivat;; // 1. Ввод значения двух целочисленных переменных
+  
+	return(0);
+}
