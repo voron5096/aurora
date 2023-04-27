9422cf47a2196f24eafbb8a1e9a6146f418d2976
voron5096 <70198995+voron5096@users.noreply.github.com>
2023-04-27 16:56:25 +0300

    Update cl_1.cpp

--- КВ_1/cl_1.cpp
+++ КВ_1/cl_1.cpp
@@ -1,10 +1,9 @@
 #include "cl_1.h"
-#include "cl_base.h"
 
 /*
 Когда 1 класс наследуется от другого, мы можем через двоеточие вызвать конструктор базового класса, для инициализации закрытых полей
 */
 
-cl_1::cl_1(cl_base* p_head, string s_name):cl_base(p_head, s_name) {
-  
-}
+cl_1::cl_1(cl_base* head_object, string s_name): cl_base(head_object, s_name) {
+
+};
