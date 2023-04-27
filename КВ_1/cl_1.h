5380b77b2c17496e3cace78777ebea2beec4b6d9
voron5096 <70198995+voron5096@users.noreply.github.com>
2023-04-27 16:56:57 +0300

    Update cl_1.h

--- КВ_1/cl_1.h
+++ КВ_1/cl_1.h
@@ -1,10 +1,15 @@
+#ifndef __CL_1_H__
+#define __CL_1_H__
 #include "cl_base.h"
-class cl_1:public cl_base { // Класс 1 наследуемый от базового класса
 
 /* У даннного класса (как и у базового класса) 1 метод - конструктор, с 2 параметрами:
 1. Указатель на главный объект
 2. Имя
 */
 
-  cl_1(cl_base* p_head, string s_name);
+class cl_1: public cl_base { // Класс 1 наследуемый от базового класса
+    public:
+        cl_1 (cl_base* head_object, string s_name);
 };
+
+#endif
