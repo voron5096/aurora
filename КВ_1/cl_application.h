227600765c00b2d9d780e4fb49d7cd705b89bf1d
voron5096 <70198995+voron5096@users.noreply.github.com>
2023-04-27 16:58:39 +0300

    Update cl_application.h

--- КВ_1/cl_application.h
+++ КВ_1/cl_application.h
@@ -1,9 +1,12 @@
+#ifndef __CL_APP_H__
+#define __CL_APP_H__
 #include "cl_base.h"
-#include "cl_1.h"
 
-class cl_application:public cl_base {
-  public:
-    cl_application(cl_base* p_head()); // конструктор (параметр указатель на головной объект)
-    void build_tree_objects(); // метод создания дерева объектов (класса cl_1)
-    int exec_app(); // метод запуска приложения (собираем все действия, которые нужно сделать с иерархией)
+class cl_application: public cl_base {
+    public:
+        cl_application(cl_base* head_object); // конструктор (параметр указатель на головной объект)
+        void build_tree_objects(); // метод создания дерева объектов (класса cl_1)
+        int exec_app(); // метод запуска приложения (собираем все действия, которые нужно сделать с иерархией)
 };
+
+#endif
