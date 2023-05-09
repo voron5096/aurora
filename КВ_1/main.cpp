78e02afdff177b661b9c6f1688e6fa2b82c55e8b
voron5096 <70198995+voron5096@users.noreply.github.com>
2023-05-09 13:04:58 +0000

    кв1 удалить

--- КВ_1/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "cl_application.h"
-
-int main () {
-	cl_application ob_cl_application(nullptr); // создание корневого объекта
-	ob_cl_application.build_tree_objects(); // конструирование системы, построение дерева объектов
-	return ob_cl_application.exec_app(); // запуск системы
-}
